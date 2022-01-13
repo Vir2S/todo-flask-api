e077228871b15ad44ae02966638cce7b841501c4
vir2s <virtualik@gmail.com>
2022-01-13 19:24:35 +0200

    configs added and python version changed

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
+app.config["SECRET_KEY"] = "thisissecret"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////./todo.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+db = SQLAlchemy(app)
 
 
-if __name__ == '__main__':
-    app.run()
+if __name__ == "__main__":
+    app.run(debug=True)
