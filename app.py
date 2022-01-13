b3b62f2fd77e2d10457e8fcfdfd2772f1a498d68
vir2s <virtualik@gmail.com>
2022-01-13 22:01:45 +0200

    User model added

--- app.py
+++ app.py
@@ -4,10 +4,22 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 app.config["SECRET_KEY"] = "thisissecret"
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:////./todo.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///todo.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, unique=True, primary_key=True)
+    public_id = db.Column(db.String(50), unique=True)
+    name = db.Column(db.String(50))
+    password = db.Column(db.String(80))
+    is_admin = db.Column(db.Boolean)
+
+
+class ToDo(db.Model):
+    pass
 
 
 if __name__ == "__main__":
