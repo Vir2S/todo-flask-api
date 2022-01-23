42744f134124426950857ce2caad2752d61bc416
vir2s <virtualik@gmail.com>
2022-01-23 17:48:45 +0200

    antipatterns refactored

--- app.py
+++ app.py
@@ -160,19 +160,20 @@ def login():
 
     user = User.query.filter_by(name=auth.username).first()
 
-    if not user:
+    if user:
+
+        if check_password_hash(user.password, auth.password):
+            token = jwt.encode(
+                {
+                    "public_id": user.public_id,
+                    "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=30)
+                },
+                app.config["SECRET_KEY"]
+            )
+
+            return jsonify({"token": token.decode("UTF-8")})
+
         return make_response("Could not verify", 401, {"WWW-Authenticate": "Basic realm='Login required!'"})
-
-    if check_password_hash(user.password, auth.password):
-        token = jwt.encode(
-            {
-                "public_id": user.public_id,
-                "exp": datetime.datetime.utcnow() + datetime.timedelta(minutes=30)
-            },
-            app.config["SECRET_KEY"]
-        )
-
-        return jsonify({"token": token.decode("UTF-8")})
 
     return make_response("Could not verify", 401, {"WWW-Authenticate": "Basic realm='Login required!'"})
 
